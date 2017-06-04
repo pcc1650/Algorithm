f4b7dc134cb0ab7d990568cd9d558384ea780e86
tonyzhao <tonyzhao1650@gmail.com>
2017-06-04 14:08:01 +0800

    add new problem

--- dp/Main.java
+++ dp/Main.java
@@ -6,12 +6,14 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
+	// 494
     public int findTargetSumWays(int[] nums, int s) {
        int sum = 0;
        for(int i: nums)
            sum += i;
        return s > sum || (s + sum) % 2 == 1 ? 0 : subsetSum(nums, (sum + s) / 2);
     }
+	// 494
     public int subsetSum(int[] nums, int s) {
         int[] dp = new int[s + 1];
         dp[0] = 1;
@@ -20,6 +22,7 @@ public class Main {
                 dp[j] += dp[j - i];
         return dp[s];
     }
+	// 523
     public boolean checkSubarraySum(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
@@ -37,6 +40,25 @@ public class Main {
                 map.put(sum, i);
         }
         return false;
+    }
+	// 516
+    public int longestPalindromeSubseq(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        // if s[i] == s[j] dp[j][i] = dp[j+1][i-1] + 2
+        // if s[i] != s[j] dp[j][i] = Math.max(dp[j+1][i], dp[j][i-1])
+        // Initialization: dp[i][i] = 1
+        for(int i = 0; i < s.length(); i++) {
+            dp[i][i] = 1;
+            for(int j = i - 1; j >= 0; j--){
+                if(s.charAt(i) == s.charAt(j)) {
+                    dp[j][i] = dp[j + 1][i - 1] + 2;
+                }
+                else {
+                    dp[j][i] = Math.max(dp[j + 1][i], dp[j][i - 1]);
+                }
+            }
+        }
+        return dp[0][s.length() - 1];
     }
     public static void main(String[] args){
         Main m = new Main();
