c18aee0dc26061767cc310ff292cf1d3cf690aa0
tonyzhao <tonyzhao1650@gmail.com>
2017-06-11 00:10:44 +0800

    add 413

--- dp/Main.java
+++ dp/Main.java
@@ -134,6 +134,19 @@ public class Main {
             sum += count[i];
         return sum;
     }
+    // 413
+    public int numberOfArithmeticSlices(int[] A) {
+        int curr = 0, sum = 0;
+        for(int i = 2; i < A.length; i++) {
+            if(A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                curr += 1;
+                sum += curr;
+            }
+            else
+                curr = 0;
+        }
+        return sum;
+    }
     public static void main(String[] args){
         Main m = new Main();
         int[] nums = new int[]{23, 2, 4, 6, 7};
