60bd4bd353433b44cec36f8d939a4840d310bbf3
tonyzhao <tonyzhao1650@gmail.com>
2017-06-11 12:56:43 +0800

    add 464

--- dp/Main.java
+++ dp/Main.java
@@ -147,6 +147,26 @@ public class Main {
         }
         return sum;
     }
+    // 464
+    public int numberOfArithmeticSlices(int[] A) {
+        int result = 0;
+        Map<Integer, Integer>[] map = new Map[A.length];
+        for(int i = 0; i < A.length; i++) {
+            map[i] = new HashMap<>();
+            for(int j = 0; j < i; j++){
+                long diff = (long)A[i] - A[j];
+                if(diff <= Integer.MIN_VALUE || diff >= Integer.MAX_VALUE){
+                    continue;     
+                }
+                int d = (int)diff;
+                int c1 = map[i].getOrDefault(d, 0);
+                int c2 = map[j].getOrDefault(d, 0);
+                result += c2;
+                map[i].put(d, c1 + c2 + 1);
+            }
+        }
+        return result;
+    }
     public static void main(String[] args){
         Main m = new Main();
         int[] nums = new int[]{23, 2, 4, 6, 7};
