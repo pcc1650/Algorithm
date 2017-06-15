3322f5582436d019c127253fda9068c8c18b7d89
tonyzhao <tonyzhao1650@gmail.com>
2017-06-15 23:31:47 +0800

    add 403 js

---
+++ dp/Main.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} stones
+ * @return {boolean}
+ */
+// 403
+var canCross = function(stones) {
+    if(stones.length === 0)
+        return true
+    let map = {}
+    for(let stone of stones){
+        map[stone] = new Set()
+    }
+    map[stones[0]].add(1)
+    for(let stone of stones){
+        for(let step of map[stone]) {
+            let reach = step + stone
+            if(reach === stones[stones.length - 1])
+                return true
+            if(map[reach] !== undefined){
+                map[reach].add(step)
+                step > 1? map[reach].add(step - 1): null
+                map[reach].add(step + 1)
+            }
+        }
+    }
+    return false
+};
