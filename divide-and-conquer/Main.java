0b58db4290eb71b90c6e3acaa56e34c873a62288
tonyzhao <tonyzhao1650@gmail.com>
2017-06-20 23:53:03 +0800

    modify 169

--- divide-and-conquer/Main.java
+++ divide-and-conquer/Main.java
@@ -1,4 +1,5 @@
 public class Solution {
+    // 169 wrong version [3, 3, 3, 4, 5, 6]
     public int majorityElement(int[] nums){
         int major = nums[0];
         int count = 1;
@@ -12,6 +13,24 @@ public class Solution {
                     count = 1;
                     major = nums[i];
                 }
+            }
+        }
+        return major;
+    }
+    // 169 
+    public int majorityElement(int[] nums) {
+        int major = nums[0];
+        int count = 1;
+        for(int i = 1; i < nums.length; i++) {
+            if(count == 0) {
+                    count = 1;
+                    major = nums[i];
+                }
+            else if(major == nums[i]){
+                count += 1;
+            }
+            else {
+                count -= 1;
             }
         }
         return major;
