3306a6d5456bf439e4b588414d6c6b67e157f2f5
tonyzhao <tonyzhao1650@gmail.com>
2017-06-22 21:43:30 +0800

    add math

---
+++ math/Main.java
@@ -0,0 +1,4 @@
+//( a + b ) % c = ( ( a % c ) + ( b % c ) ) % c
+//( a * b ) % c = ( ( a % c ) * ( b % c ) ) % c
+//( a – b ) % c = ( ( a % c ) - ( b % c ) ) % c ( see notes at bottom )
+//( a / b ) % c NOT EQUAL TO ( ( a % c ) / ( b % c ) ) % c
