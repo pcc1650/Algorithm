17f93018616023749d24b963ab6e835bf910c9de
tonyzhao <tonyzhao1650@gmail.com>
2017-06-25 19:52:25 +0800

    add 629

--- dp/Main.java
+++ dp/Main.java
@@ -293,6 +293,21 @@ public class Main {
         
         return (int)res;
     } 
+	// 629
+	// too slow
+    public int kInversePairs(int n, int k) {
+        long[][] dp = new long[n + 1][k + 1];
+        dp[1][0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j <=k; j++) {
+                for(int m = j; m >= 0 && m > (j - i); m--) {
+                    dp[i][j] += dp[i - 1][m];
+                }
+                dp[i][j] = dp[i][j] % M;
+            }
+        }
+        return (int)dp[n][k];
+    }
     public static void main(String[] args){
         Main m = new Main();
         int[] nums = new int[]{23, 2, 4, 6, 7};
