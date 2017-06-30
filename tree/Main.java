31f3e02af957248773cafc57c4e11dbb37e385d7
tonyzhao <tonyzhao1650@gmail.com>
2017-06-30 21:28:17 +0800

    add 100

--- tree/Main.java
+++ tree/Main.java
@@ -18,3 +18,16 @@ public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
     t.right = mergeTrees(t1.right, t2.right);
     return t;
 }
+// 100
+public boolean isSameTree(TreeNode p, TreeNode q) {
+    if(p == null && q == null)
+        return true;
+    if(p != null && q == null || p == null && q != null)
+        return false;
+    if(p.val == q.val){
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    else{
+        return false;
+    }
+}
