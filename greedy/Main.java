e2cfaedfc7fffcd4d2f6c50493ac48109c28ab1d
tonyzhao <tonyzhao1650@gmail.com>
2017-07-18 21:47:18 +0800

    add 621

--- greedy/Main.java
+++ greedy/Main.java
@@ -103,3 +103,26 @@ public int jump(int[] nums) {
     }
     return count;
 }
+
+// 621
+// Method 1
+public int leastInterval(char[] tasks, int n) {
+    int[] map = new int[26];
+    for(char c: tasks)
+        map[c - 'A'] += 1;
+    Arrays.sort(map);
+    int time = 0;
+    while(map[25] > 0) {
+        int i = 0;
+        while(i <= n) {
+            if(map[25] == 0)
+                break;
+            if(i < 26 && map[25 - i] > 0)
+                map[25 - i] -= 1;
+            time += 1;
+            i += 1;
+        }
+        Arrays.sort(map);
+    }
+    return time;
+}
