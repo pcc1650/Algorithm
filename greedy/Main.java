5158d67e5ab881028ea26fb091c9c84d4f4b36d4
tonyzhao <tonyzhao1650@gmail.com>
2017-07-22 14:50:24 +0800

    add 316

--- greedy/Main.java
+++ greedy/Main.java
@@ -217,3 +217,17 @@ public int findContentChildren(int[] g, int[] s) {
     }
     return count;
 }
+public String removeDuplicateLetters(String s) {
+    int[] count = new int[26];
+    int pos = 0;
+    for(int i = 0; i < s.length(); i++) {
+        count[s.charAt(i) - 'a'] += 1;
+    }
+    for(int i = 0; i < s.length(); i++) {
+        if(s.charAt(i) < s.charAt(pos))
+            pos = i;
+        if(--count[s.charAt(i) - 'a'] <= 0 )
+            break;
+    }
+    return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
+}
