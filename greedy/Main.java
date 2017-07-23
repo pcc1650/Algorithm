d5ad874f4bc0cb89005616ab120e42ff40df18a9
tonyzhao <tonyzhao1650@gmail.com>
2017-07-23 11:47:19 +0800

    add 452

--- greedy/Main.java
+++ greedy/Main.java
@@ -231,3 +231,20 @@ public String removeDuplicateLetters(String s) {
     }
     return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
 }
+// 452
+public int findMinArrowShots(int[][] points) {
+    if(points == null || points.length == 0) {
+        return 0;
+    }
+    Arrays.sort(points, (a, b) -> (a[1] - b[1]));
+    int arrPos = points[0][1];
+    int count = 1;
+    for(int i = 0 ; i < points.length; i++) {
+        if(arrPos >= points[i][0] && arrPos <= points[i][1]) {
+            continue;
+        }
+        arrPos = points[i][1];
+        count += 1;
+    }
+    return count;
+}
