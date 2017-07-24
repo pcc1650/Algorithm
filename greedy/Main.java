bb01db6c8fa973422a8b33d20e4109fd248dba7d
tonyzhao <tonyzhao1650@gmail.com>
2017-07-24 22:32:22 +0800

    add 452

--- greedy/Main.java
+++ greedy/Main.java
@@ -248,3 +248,22 @@ public int findMinArrowShots(int[][] points) {
     }
     return count;
 }
+// 402
+public String removeKdigits(String num, int k) {
+    int digits = num.length() - k;
+    char[] resChar = new char[num.length()];
+    int top = 0;
+    
+    for(int i = 0; i < num.length(); i++) {
+        char c = num.charAt(i);
+        while(top > 0 && resChar[top - 1] > c && k > 0){
+            top--;
+            k--;
+        }
+        resChar[top++] = c;
+    }
+    
+    int idx = 0;
+    while (idx < digits && resChar[idx] == '0') idx++;
+    return idx == digits? "0": new String(resChar, idx, digits - idx);
+}
