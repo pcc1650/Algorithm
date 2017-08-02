2e71992a232308fe78e66eb7c81de2746c20b9c9
tonyzhao <tonyzhao1650@gmail.com>
2017-08-02 21:38:46 +0800

    add 513 level order

--- BFS_DFS/Main.java
+++ BFS_DFS/Main.java
@@ -238,3 +238,22 @@ private void findValue(TreeNode root, int row, int[] res) {
     findValue(root.left, row + 1, res);
     findValue(root.right, row + 1, res);
 }
+// 513 level order
+public int findBottomLeftValue(TreeNode root) {
+    int res = 0;
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    while(!queue.isEmpty()) {
+        int size = queue.size();
+        for(int i = 0; i < size; i++) {
+            TreeNode node = queue.poll();
+            if(i == 0)
+                res = node.val;
+            if(node.left != null)
+                queue.add(node.left);
+            if(node.right != null)
+                queue.add(node.right);
+        }
+    }
+    return res;
+}
