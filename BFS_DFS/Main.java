cba915fa0155155cd759f22a07aad5e9bf2e2355
tonyzhao <tonyzhao1650@gmail.com>
2017-08-13 20:07:05 +0800

    add 99

--- BFS_DFS/Main.java
+++ BFS_DFS/Main.java
@@ -479,3 +479,24 @@ private boolean isBST(TreeNode root, long min, long max) {
         return false;
     return isBST(root.left, min, root.val) && isBST(root.right, root.val, max);
 }
+// 99
+TreeNode first = null;
+TreeNode second = null;
+TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+public void recoverTree(TreeNode root) {
+    traverse(root);
+    int temp = first.val;
+    first.val = second.val;
+    second.val = temp;
+}
+private void traverse(TreeNode root) {
+    if(root == null)
+        return ;
+    traverse(root.left);
+    if(first == null && prev.val >= root.val)
+        first = prev;
+    if(first != null && prev.val >= root.val)
+        second = root;
+    prev = root;
+    traverse(root.right);
+}
