518b1e1f0b179e75dbc227d67397875948e2e0f1
tonyzhao <tonyzhao1650@gmail.com>
2017-08-14 13:35:05 +0800

    add 110

--- BFS_DFS/Main.java
+++ BFS_DFS/Main.java
@@ -500,3 +500,34 @@ private void traverse(TreeNode root) {
     prev = root;
     traverse(root.right);
 }
+// 110
+// Top-down
+public boolean isBalanced(TreeNode root) {
+    if(root == null)
+        return true;
+    int left = depth(root.left);
+    int right = depth(root.right);
+    return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+}
+private int depth(TreeNode root) {
+    if(root == null)
+        return 0;
+    return Math.max(depth(root.left), depth(root.right)) + 1;
+}
+// Bottom-up
+public boolean isBalanced(TreeNode root) {
+    return dfs(root) != -1;
+}
+private int dfs(TreeNode root) {
+    if(root == null)
+        return 0;
+    int left = dfs(root.left);
+    if(left == -1)
+        return -1;
+    int right = dfs(root.right);
+    if(right == -1)
+        return -1;
+    if(Math.abs(left - right) > 1)
+        return -1;
+    return Math.max(left, right) + 1;
+}
