de0f2937fc98251a4016923c22181f9c39c043d9
tonyzhao <tonyzhao1650@gmail.com>
2017-08-18 21:47:51 +0800

    add 133

--- BFS_DFS/Main.java
+++ BFS_DFS/Main.java
@@ -647,3 +647,23 @@ public class Solution {
         return Math.max(left, right) + root.val;
     }
 }
+// 133
+public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+    HashMap<Integer, UndirectedGraphNode> map = new HashMap<>();
+    return cloneTheGraph(node, map);
+}
+private UndirectedGraphNode cloneTheGraph(UndirectedGraphNode node, HashMap<Integer, UndirectedGraphNode> map) {
+    if (node == null)
+        return null;
+    if(map.containsKey(node.label)) {
+        return map.get(node.label);
+    }
+    else {
+        UndirectedGraphNode clone = new UndirectedGraphNode(node.label);
+        map.put(node.label, clone);
+        for(int i = 0; i < node.neighbors.size(); i++) {
+            clone.neighbors.add(cloneTheGraph(node.neighbors.get(i), map));
+        }
+        return clone;
+    }
+}
