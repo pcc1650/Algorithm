432a74b93bce25d8260e21f2e0fcb00045821e0b
tonyzhao <tonyzhao1650@gmail.com>
2017-08-23 21:48:14 +0800

    add 542

--- BFS_DFS/Main.java
+++ BFS_DFS/Main.java
@@ -753,3 +753,34 @@ private void findSequence(int[] nums, int index, List<Integer> holder, Set<List<
         }
     }
 }
+// 542
+public int[][] updateMatrix(int[][] matrix) {
+    if(matrix.length == 0 || matrix[0].length == 0)
+        return null;
+    int m = matrix.length;
+    int n = matrix[0].length;
+    Queue<int[]> queue = new LinkedList<>();
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++){
+            if(matrix[i][j] == 0){
+                queue.add(new int[]{i, j});
+            }
+            else{
+                matrix[i][j] = Integer.MAX_VALUE;
+            }
+        }
+    }
+    int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    while(!queue.isEmpty()) {
+        int[] temp = queue.poll();
+        for(int[] dir: directions) {
+            int i = temp[0] + dir[0];
+            int j = temp[1] + dir[1];
+            if(i < 0 || i >= m || j < 0 || j >= n || matrix[i][j] <= matrix[temp[0]][temp[1]])
+                continue;
+            queue.add(new int[]{i, j});
+            matrix[i][j] = matrix[temp[0]][temp[1]] + 1;
+        }
+    }
+    return matrix;
+}
