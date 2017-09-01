d703d0c2c5e1c5f6243ef2b32f6351777963ecad
tonyzhao <tonyzhao1650@gmail.com>
2017-09-01 22:16:32 +0800

    312 bottom up

--- dp/Main.java
+++ dp/Main.java
@@ -315,6 +315,7 @@ public class Main {
         System.out.println(m.checkSubarraySum(nums, s));
     }
     // 312
+    // D&V and memory
     public int maxCoins(int[] nums) {
         int[] newNums = new int[nums.length + 2];
         int n = 1;
@@ -334,5 +335,23 @@ public class Main {
             ans = Math.max(ans, nums[left] * nums[i] * nums[right] + burst(memo, nums, left, i) + burst(memo, nums, i, right));
         memo[left][right] = ans;
         return ans;
+    }
+    // bottom up
+    public int maxCoins(int[] nums) {
+        int[] newNums = new int[nums.length + 2];
+        int n = 1;
+        for(int x: nums)
+            newNums[n++] = x;
+        newNums[0] = newNums[n++] = 1;
+        int[][] dp = new int[n][n];
+        for(int k = 2; k < n; k++) {
+            for(int left = 0; left < n - k; left++) {
+                int right = left + k;
+                for(int i = left + 1; i < right; i++) {
+                    dp[left][right] = Math.max(dp[left][right], newNums[left] * newNums[i] * newNums[right] + dp[left][i] + dp[i][right]);
+                }
+            }
+        }
+        return dp[0][n - 1];
     }
 }
