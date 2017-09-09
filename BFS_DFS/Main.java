2317f454ee9d7c4faf868553b4c942f7fb2960ff
zpc <tonyzhao1650@gmail.com>
2017-09-10 00:05:18 +0800

    add 301

--- BFS_DFS/Main.java
+++ BFS_DFS/Main.java
@@ -895,3 +895,30 @@ class Solution {
         return s;
     }
 }
+// 301
+public List<String> removeInvalidParentheses(String s) {
+    List<String> res = new LinkedList<>();
+    remove(s, res, 0, 0, new char[]{'(', ')'});
+    return res;
+}
+private void remove(String s, List<String> res, int last_i, int last_j, char[] par) {
+    for(int stack = 0, i = last_i; i < s.length(); i++) {
+        if(s.charAt(i) == par[0])
+            stack += 1;
+        if(s.charAt(i) == par[1])
+            stack -= 1;
+        if(stack >= 0)
+            continue;
+        for(int j = last_j; j <= i; j++) {
+            if(s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1])){
+                remove(s.substring(0, j) + s.substring(j + 1), res, i, j, par);
+            }
+        }
+        return;
+    }
+    String reversed = new StringBuilder(s).reverse().toString();
+    if(par[0] == '(')
+        remove(reversed, res, 0, 0, new char[]{')', '('});
+    else
+        res.add(reversed);
+}
