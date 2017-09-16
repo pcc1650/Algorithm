b76a2b31ddee4e946ab5f45c1400dda079c2dc1d
tonyzhao <tonyzhao1650@gmail.com>
2017-09-16 15:28:07 +0800

    add 675

--- BFS_DFS/Main.java
+++ BFS_DFS/Main.java
@@ -1152,3 +1152,54 @@ public int trapRainWater(int[][] heightMap) {
     }
     return res;
 }
+public int cutOffTree(List<List<Integer>> forest) {
+    if(forest == null || forest.size() == 0 || forest.get(0).size() == 0)
+        return -1;
+    int m = forest.size();
+    int n = forest.get(0).size();
+    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+    int[][] dirs = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++) {
+            if(forest.get(i).get(j) > 0)
+                pq.add(new int[]{i, j, forest.get(i).get(j)});
+        }
+    }
+    int[] start = new int[]{0, 0};
+    int res = 0;
+    while(!pq.isEmpty()){
+        int[] tree = pq.poll();
+        int step = minStep(forest, start, tree, m, n, dirs);
+        if(step < 0)
+            return -1;
+        res += step;
+        start[0] = tree[0];
+        start[1] = tree[1];
+    }
+    return res;
+}
+private int minStep(List<List<Integer>> forest, int[] start, int[] tree, int m, int n, int[][] dirs){
+    int step = 0;
+    boolean[][] visited = new boolean[m][n];
+    Queue<int[]> queue = new LinkedList<>();
+    queue.add(start);
+    visited[start[0]][start[1]] = true;
+    while(!queue.isEmpty()){
+        int size = queue.size();
+        for(int i = 0; i < size; i++){
+            int[] curr = queue.poll();
+            if(curr[0] == tree[0] && curr[1] == tree[1])
+                return step;
+            for(int[] dir: dirs) {
+                int nr = curr[0] + dir[0];
+                int nc = curr[1] + dir[1];
+                if(nr < 0 || nr >= m || nc <0 || nc >=n || visited[nr][nc] || forest.get(nr).get(nc) == 0)
+                    continue;
+                visited[nr][nc] = true;
+                queue.add(new int[]{nr, nc});
+            }
+        }
+        step += 1;
+    }
+    return -1;
+}
