26396088491c527aed3be069fe430e7cb5df9dc5
tonyzhao <tonyzhao1650@gmail.com>
2017-09-25 22:54:27 +0800

    add 29

--- binary-search/Main.java
+++ binary-search/Main.java
@@ -243,4 +243,22 @@ public int threeSumClosest(int[] nums, int target) {
     }
     return result;
 }
-// test
+// 29
+public int divide(int dividend, int divisor) {
+    if(divisor == 0 || (dividend == Integer.MIN_VALUE && divisor == -1))
+        return Integer.MAX_VALUE;
+    int sign = (dividend < 0 ^ divisor < 0) ? -1 : 1;
+    long dvd = Math.abs((long)dividend);
+    long dvs = Math.abs((long)divisor);
+    int res = 0;
+    while(dvd >= dvs) {
+        long temp = dvs, multiple = 1;
+        while (dvd >= (temp << 1)) {
+            temp <<= 1;
+            multiple <<= 1;
+        }
+        dvd -= temp;
+        res += multiple;
+    }
+    return sign == 1 ? res : -res;
+}
