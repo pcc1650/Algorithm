09680c130bc2978b896b7901a5836485afe2c2f8
tonyzhao <tonyzhao1650@gmail.com>
2017-09-30 16:02:29 +0800

    add 378

--- binary-search/Main.java
+++ binary-search/Main.java
@@ -262,3 +262,22 @@ public int divide(int dividend, int divisor) {
     }
     return sign == 1 ? res : -res;
 }
+// 378 also can use PriorityQueue
+public int kthSmallest(int[][] matrix, int k) {
+    int low = matrix[0][0];
+    int high = matrix[matrix.length - 1][matrix[0].length - 1] + 1;
+    while(low < high) {
+        int mid = low + (high - low) / 2;
+        int count = 0, j = matrix[0].length - 1;
+        for(int i = 0; i < matrix.length; i++) {
+            while(j >= 0 && matrix[i][j] > mid)
+                j--;
+            count += (j + 1);
+        }
+        if(count < k)
+            low = mid + 1;
+        else
+            high = mid;
+    }
+    return low;
+}
