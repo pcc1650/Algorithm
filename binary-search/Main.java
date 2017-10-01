b23bd25e8fe14d3cfd2c95a8769ac31c009a9e77
tonyzhao <tonyzhao1650@gmail.com>
2017-10-01 22:53:27 +0800

    add 287 binary

--- binary-search/Main.java
+++ binary-search/Main.java
@@ -354,3 +354,19 @@ public int findDuplicate(int[] nums) {
     }
     return -1;
 }
+// 287 binary search method
+public int findDuplicate(int[] nums) {
+    int low = 1, high = nums.length - 1;
+    while(low < high){
+        int mid = low + (high - low) / 2;
+        int count = 0;
+        for(int num: nums)
+            if(num <= mid)
+                count += 1;
+        if(count <= mid)
+            low = mid + 1;
+        else 
+            high = mid;
+    }
+    return low;
+}
