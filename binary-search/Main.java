dd9c11e0931a6a0c17b3709fbdba555d4b1f0439
tonyzhao <tonyzhao1650@gmail.com>
2017-10-05 21:53:39 +0800

    add 240

--- binary-search/Main.java
+++ binary-search/Main.java
@@ -453,3 +453,18 @@ public boolean searchMatrix(int[][] matrix, int target) {
     }
     return false;
 }
+// 240
+public boolean searchMatrix(int[][] matrix, int target) {
+    if(matrix.length == 0 || matrix[0].length == 0)
+        return false;
+    int row = 0, col = matrix[0].length - 1;
+    while(row < matrix.length && col >= 0) {
+        if(matrix[row][col] == target)
+            return true;
+        else if(matrix[row][col] < target)
+            row += 1;
+        else if(matrix[row][col] > target)
+            col -= 1;
+    }
+    return false;
+}
