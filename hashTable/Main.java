1c60e1e717ad067286ff242232698bc9ab8a2e11
tonyzhao <tonyzhao1650@gmail.com>
2017-10-06 21:01:40 +0800

    add 274

--- hashTable/Main.java
+++ hashTable/Main.java
@@ -8,3 +8,22 @@ public boolean isAnagram(String s, String t) {
     String tString = new String(tCharArray);
     return sString.equals(tString);
 }
+// 274
+public int hIndex(int[] citations) {
+    if(citations.length == 0)
+        return 0;
+    int[] count = new int[citations.length + 1];
+    for(int i = 0; i < citations.length; i++) {
+        if(citations[i] > citations.length)
+            count[citations.length] += 1;
+        else
+            count[citations[i]] += 1;
+    }
+    int total = 0;
+    for(int i = citations.length; i >= 0; i--) {
+        total += count[i];
+        if(total >= i)
+            return i;
+    }
+    return 0;
+}
