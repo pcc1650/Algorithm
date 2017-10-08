fa1616e2d9bb951ec9f02a0bac97e305af594a46
tonyzhao <tonyzhao1650@gmail.com>
2017-10-08 20:55:19 +0800

    33 another method

--- binary-search/Main.java
+++ binary-search/Main.java
@@ -484,3 +484,26 @@ public int hIndex(int[] citations) {
     }
     return citations.length - high - 1;
 }
+// 33 another method 
+public int search(int[] nums, int target) {
+    if(nums.length == 0)
+        return -1;
+    int low = 0, high = nums.length - 1;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        if(nums[mid] == target) return mid;
+        if(nums[low] <= nums[mid]){
+            if(target < nums[mid] && target >= nums[low])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        else {
+            if(target <= nums[high] && nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+    return -1;
+}
