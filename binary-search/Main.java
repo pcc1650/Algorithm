f6bfbe15a0e6767c96cbba7b4dfd612b66667aa6
tonyzhao <tonyzhao1650@gmail.com>
2017-10-24 21:21:40 +0800

    162 another method

--- binary-search/Main.java
+++ binary-search/Main.java
@@ -566,3 +566,13 @@ public int findPeakElement(int[] nums) {
     }
     return left;
 }
+// 162 sequential search
+public int findPeakElement(int[] nums) {
+    if(nums.length == 1)
+        return 0;
+    for(int i = 0; i < nums.length - 1; i++) {
+        if(nums[i] > nums[i + 1])
+            return i;
+    }
+    return nums.length - 1;
+}
