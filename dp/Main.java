aba144c50e50e4abf9b625f7bec7a1c4c0f46b37
tonyzhao <tonyzhao1650@gmail.com>
2017-10-30 20:55:41 +0800

    add 718

--- dp/Main.java
+++ dp/Main.java
@@ -404,3 +404,17 @@ public int quickSolve(int[] prices) {
     }
     return res;
 }
+// 718
+public int findLength(int[] A, int[] B) {
+    int[][] dp = new int[A.length + 1][B.length + 1];
+    int res = 0;
+    for(int i = A.length - 1; i >= 0; i--){
+        for(int j = B.length - 1; j >= 0; j--) {
+            if(A[i] == B[j]){
+                dp[i][j] = dp[i + 1][j + 1] + 1;
+                res = Math.max(res, dp[i][j]);
+            }
+        }
+    }
+    return res;
+}
