76249e01f832faceaa2d514f1c28179b4b5742b9
tonyzhao <tonyzhao1650@gmail.com>
2017-11-18 11:04:23 +0800

    modify 650 code

--- dp/Main.java
+++ dp/Main.java
@@ -579,7 +579,7 @@ public int minSteps(int n) {
     for(int i = 2; i <= n; i++) {
         for(int j = i / 2; j > 1; j--) {
             if(i % j == 0){
-                dp[i] = Math.min(dp[i], dp[j] + (i / j));
+                dp[i] = dp[j] + (i / j);
                 break;
             }
         }
